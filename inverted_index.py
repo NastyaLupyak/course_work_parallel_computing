c930e211852aec518ec51c6f9d3fe0fed81b8529
NastyaLupyak <44192883+NastyaLupyak@users.noreply.github.com>
2020-05-12 20:12:56 +0300

    feat: add new functions "inverted_index", "edit"

--- inverted_index.py
+++ inverted_index.py
@@ -1,37 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from multiprocessing import Process
 import os
 from time import time
 
+
 class NewProcess(Process):
 
-    def __init__(self, folder):
+    def __init__(self, folder, files):
         Process.__init__(self)
         self.folder = folder
         self.process_dictionary = {}
+        self.files = files
 
     def run(self):
-        self.open_files()
+        for file in self.files:
+            with open(self.folder + file) as file_handler:
+                for text in file_handler:
+                    text = text.split(' ')
+                    text = [self.edit(x) for x in text]
+                    self.inverted_index(text, file)
         return self.process_dictionary
 
-    def inverted_index(self):
-        pass
+    def edit(self, word):
+        symb = [
+            ',',
+            '!',
+            '/',
+            '?',
+            '.',
+            '(',
+            ')',
+            "'",
+            '"',
+            ]
+        word = word.lower()
+        if word:
+            for s in symb:
+                if s in word:
+                    word = word.replace(s, '')
+        return word
+
+    def inverted_index(self, string, doc_name):
+        index = 0
+        for i in string:
+            if i not in self.process_dictionary.keys():
+                self.process_dictionary[i] = [(doc_name, index)]
+            else:
+                self.process_dictionary[i] += [(doc_name, index)]
+            index += 1
 
 
- if __name__ == '__main__':
+if __name__ == '__main__':
 
     folder = 'C:/Users/lupya/Desktop/test/'
-    files = os.listdir(folder) 
+    files = os.listdir(folder)
     num_of_processes = 1
     processes = []
 
     for i in range(num_of_processes):
-        p = NewProcess(folder)
+        p = NewProcess(folder, files)
         processes.append(p)
-
 
     for process in processes:
         process.start()
-
-
-
-    
